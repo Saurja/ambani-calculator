5bc124d574d2e536d87d4c82399dce38e522b57c
Saurja <saurjaghoshofficial@gmail.com>
2021-04-14 16:15:04 +0530

    Ambani

--- src/App.js
+++ src/App.js
@@ -7,18 +7,18 @@ function App() {
             <article>
                 <p>This is how much Mukesh Ambani...</p>
                 <output>
-                    <span class="symbol">₹</span>
-                    <span class="number u8"></span>
-                    <span class="number u7"></span>
-                    <span class="number u6"></span>
-                    <span class="symbol u6">,</span>
-                    <span class="number u5"></span>
-                    <span class="number u4"></span>
-                    <span class="number u3"></span>
-                    <span class="symbol">,</span>
-                    <span class="number u2"></span>
-                    <span class="number u1"></span>
-                    <span class="number u0"></span>
+                    <span className="symbol">₹</span>
+                    <span className="number u8"></span>
+                    <span className="number u7"></span>
+                    <span className="number u6"></span>
+                    <span className="symbol u6">,</span>
+                    <span className="number u5"></span>
+                    <span className="number u4"></span>
+                    <span className="number u3"></span>
+                    <span className="symbol">,</span>
+                    <span className="number u2"></span>
+                    <span className="number u1"></span>
+                    <span className="number u0"></span>
                 </output>
                 <p>...since you started reading this page.</p>
                 <img id="AmbaniPic" src={ambani} alt="Muskesh Ambani" />
