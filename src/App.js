69b5f0d8004a5ee77170a3cdb80754979d421592
Saurja <saurjaghoshofficial@gmail.com>
2021-04-14 15:51:05 +0530

    Make me rich

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ambani from "./mukesh-ambani.jpg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <article>
+                <p>This is how much Mukesh Ambani...</p>
+                <output>
+                    <span class="symbol">₹</span>
+                    <span class="number u8"></span>
+                    <span class="number u7"></span>
+                    <span class="number u6"></span>
+                    <span class="symbol u6">,</span>
+                    <span class="number u5"></span>
+                    <span class="number u4"></span>
+                    <span class="number u3"></span>
+                    <span class="symbol">,</span>
+                    <span class="number u2"></span>
+                    <span class="number u1"></span>
+                    <span class="number u0"></span>
+                </output>
+                <p>...since you started reading this page.</p>
+                <img id="AmbaniPic" src={ambani} alt="Muskesh Ambani" />
+            </article>
+            <footer>
+                <a href="https://github.com/Saurja">Made By @Saurja Ghosh</a>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
